37902b37d3e2f4814917bfd10b8259a2b228b699
ValeriyKryatov <Kwaler@mail.ru>
2024-01-19 10:57:22 +0300

    PetShelterTGBot. First commit

---
+++ src/main/java/PetShelterTGBot/PetShelterTgBotApplication.java
@@ -0,0 +1,13 @@
+package PetShelterTGBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShelterTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShelterTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PetShelterTGBot/PetShelterTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package PetShelterTGBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
